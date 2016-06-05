8f07b61d149158d2d6a4a032e261b5594711c6bf
Moritz Grauel <mo@notadomain.com>
2016-06-05 14:16:55 +0200

    with headers

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,19 +1,21 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE TypeOperators #-}
 module Lib
     ( startApp
     ) where
 
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Control.Monad.Reader
-import Servant
+import           Control.Monad.Reader
+import           Data.Maybe               (fromMaybe)
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
 
 newtype Greet = Greet String deriving (Show)
 type Handler' a = ReaderT Greet Handler a
 
 type API = "a" :> Get '[JSON] String
   :<|> "b" :> Get '[JSON] Int
+  :<|> "c" :> Header "Accept" String :> Get '[PlainText] (Headers '[Header "X-Foo" String] String)
 
 startApp :: IO ()
 startApp = run 8080 (app (Greet "Hello"))
@@ -27,9 +29,9 @@ app g = serve api enteredServer
 api :: Proxy API
 api = Proxy
 
-server :: Handler' String :<|> Handler' Int
 server = aHandler'
   :<|> bHandler'
+  :<|> cHandler
 
 aHandler' :: Handler' String
 aHandler' = do
@@ -39,3 +41,7 @@ aHandler' = do
 
 bHandler' :: Handler' Int
 bHandler' = return 1
+
+cHandler :: Maybe String -> Handler' (Headers '[Header "X-Foo" String] String)
+cHandler x = let f = fromMaybe "none" x in
+               return $ addHeader "bar" f
